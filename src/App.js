80a8f93659ff877f3bf8128cee38367e5950cb76
Raúl Sánchez <raul.sanchez@trivago.com>
2021-05-07 16:18:07 +0200

    Tutorial completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Admin, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import { UserList } from "./users";
+import { PostEdit, PostList, PostCreate } from "./posts";
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import Dashboard from "./Dashboard";
+import authProvider from "./authProvider";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// TODO define real data provider
+// https://marmelab.com/react-admin/Tutorial.html#connecting-to-a-real-api
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
+const App = () => (
+    <Admin
+        dashboard={Dashboard}
+        dataProvider={dataProvider}
+        authProvider={authProvider}
+    >
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+        <Resource name="users" list={UserList} icon={UserIcon}/>
+    </Admin>
+);
 
 export default App;
